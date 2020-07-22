6f4c271c239c02219a9212ded100853eff806fd1
Ayaka-M-tech <67506928+Ayaka-M-tech@users.noreply.github.com>
2020-07-22 17:54:07 +0900

    Create No1.cs

---
+++ No1.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        var N = int.Parse(Console.ReadLine());
+        var M = int.Parse(Console.ReadLine());
+
+        Console.Write("{0}",N/10 * M);
+
+        Console.Write("\n");
+    }
+}
